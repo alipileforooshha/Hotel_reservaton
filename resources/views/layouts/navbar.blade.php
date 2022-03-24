14f96ce040f7124770b2726439e5c697d1180ddd
ali484 <micahelwwww@gmail.com>
2022-03-25 00:05:15 +0430

    Add Navbar Search section And Featured Hotels

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('./css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('./css/style.css')}}">
+    <title>Document</title>
+</head>
+<body style="direction: rtl; background-color: rgb(241, 241, 241);">
+    <nav class="navbar navbar-expand-sm d-flex bg-gray justify-content-between align-items-center mx-3">
+        <ul class="d-flex justify-content-center align-items-center navbar-nav">
+            <li class="nav-item mx-1">
+                <a class="nav-link h5 text-dark" href="">صفحه اصلی</a>
+            </li>
+            <li class="nav-item mx-1">
+                <a class="nav-link h5 text-dark" href="">جستجوی هتل</a>
+            </li>
+            <li class="nav-item mx-1">
+                <a class="nav-link h5 text-dark" href="">صفحه اصلی</a>
+            </li>
+            <li class="nav-item mx-1">
+                <a class="nav-link h5 text-dark" href="">جستجوی هتل</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav justify-content-center align-items-center">
+            <li class="nav-item">
+                <a class="nav-link h5" href="">ورود/ثبت نام</a>
+            </li>
+        </ul>
+    </nav>
+    <hr class="m-0">
+</body>
+</html>
